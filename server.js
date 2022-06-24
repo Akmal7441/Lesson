7a6b0bd10710cfda086798c3e66c073b341db2d7
Akmal7441 <98477405+Akmal7441@users.noreply.github.com>
2022-06-24 19:57:37 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const lessons = [
+  {photo:"https://avatars.mds.yandex.net/i?id=dbd4609ec1e2bdf0e8b7efb5b397af53-4834372-images-thumbs&n=13" ,name: "Node js", id: 1 },
+  {photo:"https://avatars.mds.yandex.net/i?id=38975f688d02b1a79bae344fa393bfe5-4824023-images-thumbs&n=13", name: "React js", id: 2 },
+  {photo:"https://avatars.mds.yandex.net/i?id=893750cf9cd017aa0948fc08f888b38e-5236667-images-thumbs&n=13",name: "JavaScript", id: 3 },
+]
+
+module.exports = lessons;
